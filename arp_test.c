0a032ff93c1c5d92311adcb6a7715e9debe63fc2
wnstjr93 <kjsgood21@gmail.com>
2017-07-29 17:48:03 +0900

    why wrote the ip? ip,arp-3Layer

--- arp_test.c
+++ arp_test.c
@@ -1,67 +1,24 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
 #include <arpa/inet.h>
 
-#define ETH_ALEN 6
+#define MAC_BUF_LEN 20
 
-typedef struct ether_header // ..xx/net/ethernet.h
-{ 
-	u_int8_t ether_dhost[ETH_ALEN]; //1BYTE
-   	u_int8_t ether_shost[ETH_ALEN]; //1BYTE
-	u_int16_t ether_type //2BYTE
-}ether_header;
+void get_mac(void);
+typedef struct ether_header ether_header; //ethernet.h
+typedef struct ether_arp ether_arp; //if_ether.h
 
-typedef struct iphdr // ..xx/linux/ip.h
-{
-	u_int8_t ihl:4 //header length 4bit
-	u_int8_t version:4 //version 4bit
-	u_int8_t tos;		//type of service
-	u_int16_t tot_len;	//total length 2byte
-	u_int16_t id;		//identification
-	u_int16_t frag_off;	//fragment offset field
-	u_int8_t ttl;		//time to live
-	u_int8_t protocol; //protocol
-	u_int16_t check;	//check sum
-	struct in_addr ip_src;//src ip
-	struct in_addr ip_dst;
-}iphdr;
-
-typedef struct ether_arp
-{
-	u_int16_t ar_hrd;
-	u_int16_t ar_pro;
-	u_int8_t ar_hln;
-	u_int8_t ar_pln;
-	u_int16_t ar_op;
-	u_int8_t arp_src_mac[ETHER_ALEN];
-	struct in_addr arp_src_ip;
-	u_int8_t arp_dst_mac[ETHER_ALEN];
-	struct in_addr arp_dst_ip;
-}ether_arp;
-
-void get_mac(u_char *packet)
-{
-
-	FILE *fp;
-	char mac[6];
-	ether_header *ether;
-	ether=(ether_header *)packet;
-	ether->ether_dhost
-	/*if(fD=fopen("/sys/class/net/eth0/address","r"))
-	{
-		fp=strtol(fp,NULL,16);
-		fget(ether->ether_dhost,ETH_ALEN,fp);
-
-	}*?
-}
+const char *kStringMacAddree = "/sys/class/net/eth0/address";
 
 int main(int argc, char *argv[])
 {
+
 	pcap_t *handle;			/* Session handle */
 	char *dev;			/* The device to sniff on */
 	char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
@@ -70,28 +27,35 @@ int main(int argc, char *argv[])
 	bpf_u_int32 mask;		/* Our netmask */
 	bpf_u_int32 net;		/* Our IP */
 	struct pcap_pkthdr header;	/* The header that pcap gives us */
-	const u_char *packet;		/* The actual packet */
+	u_char *packet;		/* The actual packet */
 
 	/* Define the device */
+/*
 	dev = pcap_lookupdev(errbuf);
 	if (dev == NULL) {
 		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
 		return(2);
 	}
+*/
 	/* Find the properties for the device */
+/*
 	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
 		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
 		net = 0;
 		mask = 0;
 	}
+*/
 
 	/* Open the session in promiscuous mode */
-	handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
+/*
+   handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
 	if (handle == NULL) {
 		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
 		return(2);
 	}
+*/
 	/* Compile and apply the filter */
+/*
 	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
 		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
 		return(2);
@@ -100,9 +64,47 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
 		return(2);
 	}
+*/
+	get_mac();
+	make_eth(packet);
+	
 
-
-	pcap_next_ex(handle, &header,&packet);
-	pcap_close(handle);
+	
+//	pcap_next_ex(handle, &header,&packet);
+//	pcap_close(handle);
 	return(0);
+}
+
+void get_mac(void)
+{
+	FILE *fp = fopen(kStringMacAddree, "r");
+	char mac[MAC_BUF_LEN] = {'\0', };
+	ether_header ether;
+
+	fgets(mac, MAC_BUF_LEN, fp);
+	for(int i=0;i<ETH_ALEN;i++){
+		ether.ether_dhost[i] = strtol(&mac[i*3], NULL, 16);
+		printf("%02X :",ether.ether_dhost[i]);
+	}
+
+	fclose(fp);
+}
+
+void make_eth(u_char *packet)
+{
+	ether_header *ether;
+	ether=(ether_header *)packet;
+	memset(ether->ether_shost,0xff,ETH_ALEN);
+	ether_type=ETHERTYPE_ARP;
+}
+
+void make_arp(u_char *packet)
+{
+	eth_arp *arp;
+	packet+=sizeof(ether_header);
+	arp=(eth_arp *)packet;
+
+	
+
+
 }
